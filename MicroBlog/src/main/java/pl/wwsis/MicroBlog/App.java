65cbf114d14c0fb2f9a8459e755e7b92671482e2
jkk21 <kulikjolanta@tlen.pl>
2021-01-03 10:47:36 +0100

    starting app

--- MicroBlog/src/main/java/pl/wwsis/MicroBlog/App.java
+++ MicroBlog/src/main/java/pl/wwsis/MicroBlog/App.java
@@ -1,16 +1,19 @@
 package pl.wwsis.MicroBlog;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-/**
- * Hello world!
- *
- */
+
+
+@SpringBootApplication
+//@ComponentScan(basePackages = "pl.wwsis.MicroBlog")
 public class App 
 {
     public static void main( String[] args )
     {
     	
-    	System.out.println( "Hello World!" );
+    	SpringApplication.run(App.class, args);
           
     }
 }
