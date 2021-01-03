07107ec786a34addb80a19ef6263f30e658469c0
jkk21 <kulikjolanta@tlen.pl>
2021-01-03 11:06:41 +0100

    configuration

--- MicroBlog/src/main/java/pl/wwsis/MicroBlog/App.java
+++ MicroBlog/src/main/java/pl/wwsis/MicroBlog/App.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
-@ComponentScan(basePackages = "pl.wwsis.MicroBlog")
+//@ComponentScan(basePackages = "pl.wwsis.MicroBlog")
 public class App 
 {
     public static void main( String[] args )
