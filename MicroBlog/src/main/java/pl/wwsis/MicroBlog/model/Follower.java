83234c744c99cb2540a287717838f81d237944b2
jkk21 <kulikjolanta@tlen.pl>
2021-01-17 18:13:21 +0100

    constraints added

--- MicroBlog/src/main/java/pl/wwsis/MicroBlog/model/Follower.java
+++ MicroBlog/src/main/java/pl/wwsis/MicroBlog/model/Follower.java
@@ -1,50 +1,41 @@
 package pl.wwsis.MicroBlog.model;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.IdClass;
 import javax.persistence.Table;
 import javax.persistence.Column;
-import lombok.AllArgsConstructor;
-import lombok.AccessLevel;
 import lombok.Data;
 
 @Data
-//@AllArgsConstructor
-@Entity @IdClass(FollowerId.class)
+@Entity
+@IdClass(FollowerId.class)
 
-
-@Table(name="FOLLOWER")
+@Table(name = "FOLLOWER")
 public class Follower {
 
-@Id
-    @Column(name = "userId")
-    private int id;
-    
-@Id
-    @Column(name = "followsUserId")
-    private int followsUserId;
+	@Id
+	@Column(name = "userId")
+	private int id;
 
-    public int getId() {
-	return id;
-}
+	@Id
+	@Column(name = "followsUserId")
+	private int followsUserId;
 
+	public int getId() {
+		return id;
+	}
 
-public int getFollowsUserId() {
-	return followsUserId;
-}
+	public int getFollowsUserId() {
+		return followsUserId;
+	}
 
-
-	public Follower() {}
-
+	public Follower() {
+	}
 
 	public Follower(int id, int followsUserId) {
 		this.id = id;
 		this.followsUserId = followsUserId;
 	}
-    
-    
-    
+
 }
